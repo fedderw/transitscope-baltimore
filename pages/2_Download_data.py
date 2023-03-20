85d76eb027727bc9e78ccf5b0e9313d7996d01e1
William Fedder <fedderw@gmail.com>
2023-03-20 19:41:39 -0400

    simplify data download

--- pages/2_Download_data.py
+++ pages/2_Download_data.py
@@ -16,7 +16,7 @@ def convert_df(df):
     return df.to_csv().encode("utf-8")
 
 
-rides = get_rides()
+rides = get_rides()[["route","date", "ridership","ridership_weekday","ridership_weekday_2019"]].rename(columns={"ridership_weekday_2019":"ridership_weekday_2019_baseline","business_days":"n_weekdays"})
 
 st.markdown("## Ridership data by route")
 dataframe = rides
@@ -24,7 +24,7 @@ filtered_dataframe = dataframe_explorer(dataframe)
 st.dataframe(filtered_dataframe, use_container_width=True)
 csv = convert_df(filtered_dataframe)
 st.download_button(
-    label="Download full dataset as CSV",
+    label="Download filtered dataset as CSV",
     data=csv,
     file_name="mta_bus_ridership.csv",
     mime="text/csv",
