84ca510cb8a94242119c5c0f79947674b3436a05
Will Fedder <fedderw@gmail.com>
2023-05-16 23:57:02 -0400

    add walksheds.py

---
+++ app/download_walksheds.py
@@ -0,0 +1,8 @@
+import geopandas as gpd
+import janitor
+# Define the URL of the ArcGIS polygon layer in GeoJSON format
+url = "https://services.arcgis.com/njFNhDsUCentVYJW/arcgis/rest/services/Existing_Walkshed_Areas/FeatureServer/0/query?where=1%3D1&outFields=*&f=geojson"
+
+# Read the GeoJSON data into a GeoDataFrame
+gdf = gpd.read_file(url).clean_names()
+gdf.to_file("../data/mta_rail_walksheds.geojson", driver="GeoJSON")
